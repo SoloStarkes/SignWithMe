5cca98f86f3a5be0a3d91102be82ab426c274376
Abel Chacko Mathew <achusep19@gmail.com>
2024-11-19 13:27:23 -0500

    Updated translator functionality to tokenize a word to a map of characters

--- backend/translator/main.py
+++ backend/translator/main.py
@@ -1,8 +1,19 @@
-def main():
+# tokenize word to convert each word into a queue of characters
+# data structure must have fast access and insert
+def tokenize_input(inputData):
+    words_list = []
+    for word in inputData.split():
+        words_list.append(list(word))
+    return words_list
+
+
+def translate():
     # get the text from the input div in the page
     # each char must be converted to a letter form the
-    print("Hello World!")
+    word = "Our goal at Sign With Me is to cultivate understanding and inclusion " \
+           "through the power of sign language."
+    word_to_char_map = tokenize_input(word)
+    print(word_to_char_map)
 
 
-if __name__ == "__main__":
-    main()
+translate()
