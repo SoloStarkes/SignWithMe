89d4e41f2d43811140e828579ed1ab82c0b474a9
Abel Chacko Mathew <achusep19@gmail.com>
2024-11-22 13:29:05 -0500

    authentication fixes

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+
+router.get('/authenticate', async (req, res) => {
     // console.log('Got request: ' + req.statusCode);
     if (req.headers.authorization){
         const base64Credentials = req.headers.authorization.split(' ')[1];
         const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
-        const [username, password] = credentials.split(":");
-        console.log(username + ":" + password);
-        // first search for usernam in collection users
+        const [email, password] = credentials.split(":");
+        console.log(email + ":" + password);
+        // first search for username in collection users
+        // if the username isn't found, send appropriate error
+        // store sessionid in redis
     }
 });
+
+router.get('/', (req, res) => {
+    // check if the sessionId + userId is in the Redis database
+    // if session is already in
+})
+
+router.post('/create', (req, res) => {
+    console.log('Creating a User!! ');
+
+})
 
 module.exports = router;
